f31e4fd8d59da5509b8a03dee3b93d1430d2893f
ingrid-mo <morales.ingrid.i20@gmail.com>
2023-11-02 20:41:37 -0300

    agregue alt

--- src/Componets/HomePage.jsx
+++ src/Componets/HomePage.jsx
@@ -14,7 +14,7 @@ Contactanos
       </div>
 
      <div className="BigHero_img">
-     <img src="https://firebasestorage.googleapis.com/v0/b/weather-7f851.appspot.com/o/Group%202%20(1).png?alt=media&token=fd53749b-3e5a-4c3c-92eb-3c59ce085ff3&_gl=1*1wzwpyy*_ga*MTExNzIyMzk2OC4xNjk4ODgyMDUx*_ga_CW55HF8NVT*MTY5ODkzNjU5NC4zLjEuMTY5ODkzNjU5Ni41OC4wLjA." alt="" />
+     <img src="https://firebasestorage.googleapis.com/v0/b/weather-7f851.appspot.com/o/Group%202%20(1).png?alt=media&token=fd53749b-3e5a-4c3c-92eb-3c59ce085ff3&_gl=1*1wzwpyy*_ga*MTExNzIyMzk2OC4xNjk4ODgyMDUx*_ga_CW55HF8NVT*MTY5ODkzNjU5NC4zLjEuMTY5ODkzNjU5Ni41OC4wLjA." alt="torta osito" />
      </div>
      
       </div>
