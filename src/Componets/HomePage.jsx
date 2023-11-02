fa67c3fe336b6ab3441249907c66f7c11c64bcf0
ingrid-mo <morales.ingrid.i20@gmail.com>
2023-11-02 20:40:54 -0300

    agregue alts y botones

--- src/Componets/HomePage.jsx
+++ src/Componets/HomePage.jsx
@@ -1,17 +1,22 @@
 
-
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div>
       <div  className="BigHero" >
-      <div className="text-light">
+      <div className="text-light bighero_text">
       <h1>Bienvenido a Happy Cake </h1>
       <p>El lugar de los pasteles felices</p>
+      <Button variant="outline-light"><Link to= "/contact" className="text-light text-decoration-none"> 
+Contactanos
+</Link></Button>{' '}
       </div>
+
      <div className="BigHero_img">
      <img src="https://firebasestorage.googleapis.com/v0/b/weather-7f851.appspot.com/o/Group%202%20(1).png?alt=media&token=fd53749b-3e5a-4c3c-92eb-3c59ce085ff3&_gl=1*1wzwpyy*_ga*MTExNzIyMzk2OC4xNjk4ODgyMDUx*_ga_CW55HF8NVT*MTY5ODkzNjU5NC4zLjEuMTY5ODkzNjU5Ni41OC4wLjA." alt="" />
      </div>
-
+     
       </div>
  
     </div>
--- src/Componets/Navegations.jsx
+++ src/Componets/Navegations.jsx
@@ -1,6 +1,6 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 
 function Navegation() {
@@ -8,19 +8,27 @@ function Navegation() {
     <>
       <Navbar bg="danger" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand>Navbar</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/" className="text-light mt-2 pe-2 text-decoration-none">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/weather-7f851.appspot.com/o/Group%2039%20(1).png?alt=media&token=4714770b-2eae-49bd-874d-a2bb3c5dcdc1&_gl=1*5k6t8b*_ga*MTExNzIyMzk2OC4xNjk4ODgyMDUx*_ga_CW55HF8NVT*MTY5ODk2NjQ1OC40LjEuMTY5ODk2NzkyMy41My4wLjA."
+                alt="logo happy cake"
+              />
+            </Link>
+          </Navbar.Brand>
           <Nav className="me-auto">
-<Link to= "/" className="text-light mt-2 pe-2 text-decoration-none"> 
-Home
-</Link>
-<Link to= "/contact" className="text-light mt-2 pe-2 text-decoration-none"> 
-contact
-</Link>
+            <Link to="/" className="text-light mt-2 pe-2 text-decoration-none">
+              Home
+            </Link>
+            <Link
+              to="/contact"
+              className="text-light mt-2 pe-2 text-decoration-none"
+            >
+              contact
+            </Link>
           </Nav>
         </Container>
       </Navbar>
-
- 
     </>
   );
 }
