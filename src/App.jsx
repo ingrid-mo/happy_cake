7b274ab22d59eb9d4af2d6fc199d5d2e80823454
ingrid-mo <morales.ingrid.i20@gmail.com>
2023-11-05 16:50:11 -0300

    agregue carpeta view

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Navegation from "./Componets/Navegations"
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./Componets/HomePage";
+import HomePage from "./view/HomePage";
 import NotFound from "./Componets/NotFound";
-import Contact from "./Componets/Contact";
+import Contact from "./view/Contact";
+
 import "./app.css";
 
 const App = () => {
